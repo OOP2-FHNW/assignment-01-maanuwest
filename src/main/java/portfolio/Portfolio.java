3b3047d0ccb9316b7f8357111f68b3f3a50fa467
Manuel <wustmanuel@gmail.com>
2020-03-03 16:55:48 +0100

    all tests running green

--- src/main/java/portfolio/Portfolio.java
+++ src/main/java/portfolio/Portfolio.java
@@ -1,5 +1,57 @@
 package portfolio;
 
-public class Portfolio {
-    //TODO Complete this implementation and make it generic
+import portfolio.investments.Investment;
+import portfolio.investments.Share;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Portfolio<T extends Investment>{
+    private List<T> portfolio = new ArrayList<>();
+
+    public boolean contains(T investment){
+        return portfolio.contains(investment);
+    }
+
+    public void buy(T t){
+        for(int i=0; i<portfolio.size();i++){
+            if ( portfolio.get(i).equals(t) ) {
+                double oldCount = portfolio.get(i).getCount();
+                double newCount = t.getCount();
+                double toAdd = oldCount + newCount;
+                portfolio.get(i).setCount(toAdd);
+                return;
+            }
+        }
+        portfolio.add(t);
+    }
+
+    public void sell(String s, double amount){
+            if (getShare(s) != null) {
+                T t = getShare(s);
+                double oldCount = t.getCount();
+                double newCount = oldCount - amount;
+
+                if(amount == oldCount){
+                    portfolio.remove(t);
+                    t.setCount(0);
+                }else if(amount < oldCount) {
+                    t.setCount(newCount);
+                }else{
+                    //TODO: error sell amount is bigger than portfolio
+                    return;
+                }
+            }
+    }
+
+    public T getShare(String s){
+        for(int i=0; i<portfolio.size();i++){
+            if ( portfolio.get(i).getTitel().equals(s) ) {
+                return portfolio.get(i);
+            }
+        }
+        return null;
+    }
+
 }
